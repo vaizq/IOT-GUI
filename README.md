b21be3e9b349c19bbdc8d4d9ff924bd054749892
Vaige <68817622+vaizq@users.noreply.github.com>
2023-12-03 21:07:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Base GUI project setup with SDL2 and OpenGL
+
+C++ GUI project template with [SDL2](https://www.libsdl.org) and [Dear ImGUI](https://github.com/ocornut/imgui) with
+OpenGL 4.1.
+
+![Image of the example app.](example-app.png)
+
+## How to get started
+
+After using this template to create your own repository, it is time to first run and then customize!
+
+- Start here: [Quick Start](docs/QuickStart.md)
+- Continue here: [Usage Guide](docs/README.md)
+
+## Other versions
+
+There is also a [SDL2 version using the SDL2 Renderer](https://github.com/MartinHelmut/cpp-gui-template-sdl2) instead of
+OpenGL specifically, but it does not support multi-viewports or detached widgets.
+
+## Disclaimer
+
+Special acknowledgment to the amazing open source font [Manrope](https://manropefont.com).
---
+++ src/core/Core/Application.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <SDL.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "Core/Window.hpp"
+
+namespace App {
+
+enum class ExitStatus : int { SUCCESS = 0, FAILURE = 1 };
+
+class Application {
+ public:
+  explicit Application(const std::string& title);
+  ~Application();
+
+  Application(const Application&) = delete;
+  Application(Application&&) = delete;
+  Application& operator=(Application other) = delete;
+  Application& operator=(Application&& other) = delete;
+
+  ExitStatus run();
+  void stop();
+
+  void on_event(const SDL_WindowEvent& event);
+  void on_minimize();
+  void on_shown();
+  void on_close();
+
+ private:
+  ExitStatus m_exit_status{ExitStatus::SUCCESS};
+  std::unique_ptr<Window> m_window{nullptr};
+
+  bool m_running{true};
+  bool m_minimized{false};
+  bool m_show_some_panel{true};
+  bool m_show_debug_panel{false};
+  bool m_show_demo_panel{false};
+};
+
+}  // namespace App
